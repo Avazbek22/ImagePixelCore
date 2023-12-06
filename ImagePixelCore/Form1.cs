144898d407db696a56cb46189396b340c08453f8
Avazbek Olimov <84025676+Avazbek22@users.noreply.github.com>
2023-12-06 22:15:00 +0300

    Stable Blur and version control

--- ImagePixelCore/Form1.cs
+++ ImagePixelCore/Form1.cs
@@ -4,17 +4,25 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing.Imaging;
 using AForge.Imaging.Filters;
-using Tesseract;
 namespace ImagePixelCore
 {
 
     public partial class Form1 : Form
     {
         private List<Bitmap> _bitmaps = new List<Bitmap>(100);
-        private Random rnd = new Random(DateTime.Now.Millisecond);
-        private Bitmap? currentImage = null;
-        private readonly Bitmap? originalImage = null;
-        private string imagePath;
+        private Random _rnd = new Random(DateTime.Now.Millisecond);
+        private Bitmap? _currentImage = null;
+        private Bitmap? _originalImage = null;
+        private string _imagePath;
+        private ImageAction _imageAction;
+
+        private enum ImageAction
+        {
+            Opened,
+            Deleted,
+            PixelsDeleted,
+            ImageBlured
+        }
         public Form1()
         {
             InitializeComponent();
@@ -35,9 +43,11 @@ namespace ImagePixelCore
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                imagePath = openFileDialog1.FileName;
-                currentImage = new Bitmap(imagePath);
-                await Task.Run(() => { pictureBox1.Image = currentImage; });
+                _imagePath = openFileDialog1.FileName;
+                _currentImage = new Bitmap(_imagePath);
+                _originalImage = _currentImage;
+                await Task.Run(() => { pictureBox1.Image = _currentImage; });
+                _imageAction = ImageAction.Opened;
             }
         }
 
@@ -50,22 +60,24 @@ namespace ImagePixelCore
                 MessageBox.Show("Delete this image?", "Program", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
                 UpdateForm();
+                _imageAction = ImageAction.Deleted;
             }
         }
 
 
         private async void deletePixelsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            splitContainer1.Panel2Collapsed ^= true;
+            if(_imageAction != ImageAction.PixelsDeleted)
+                splitContainer1.Panel2Collapsed ^= true;
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
             menuStrip1.Enabled = false;
             UpdateForm();
-            await Task.Run(() => { DeletePixels(currentImage!, trackBar1.Maximum); });
+            await Task.Run(() => { DeletePixels(_currentImage!, trackBar1.Maximum); });
             (menuStrip1.Enabled, trackBar1.Enabled) = (true, true);
             sw.Stop();
-            Text = $"Прошедшее время: {sw.Elapsed.ToString().Remove(10)}";
+            Text = $"Time passed: {sw.Elapsed.ToString().Remove(10)}";
 
             void DeletePixels(Bitmap bitmap, int steps)
             {
@@ -76,7 +88,7 @@ namespace ImagePixelCore
                     int length = array.Length - 1;
                     for (int i = 0, rand; i < length; i++)
                     {
-                        rand = rnd.Next(i, length);
+                        rand = _rnd.Next(i, length);
 
                         (array[rand], array[i]) = (array[i], array[rand]); // Swap
                     }
@@ -84,15 +96,13 @@ namespace ImagePixelCore
                     return array;
                 }
 
-                int[] indexes = Enumerable.Range(0, totalPixelsCount + 1).ToArray();
-
-                Shuffle(indexes);
+                int[] indexes = Shuffle(Enumerable.Range(0, totalPixelsCount + 1).ToArray());
 
                 Bitmap currentBitmap = new Bitmap(bitmap.Width, bitmap.Height);
 
                 for (int i = 1, coor, j, bw = bitmap.Width; i < steps; i++)
                 {
-                    for (j = 0, coor = indexes[i * pixelsInStep + j]; j < pixelsInStep; j++)
+                    for (j = 0; j < pixelsInStep; j++)
                     {
                         coor = indexes[i * pixelsInStep + j];
                         currentBitmap.SetPixel(x: (coor % bw), y: (coor / bw), bitmap.GetPixel(coor % bw, coor / bw));
@@ -100,38 +110,64 @@ namespace ImagePixelCore
 
                     _bitmaps.Add((Bitmap)currentBitmap.Clone());
 
-                    this.Invoke(new Action(() => { Text = $"{i} %"; }));
-                    this.Invoke(new Action(() => { pictureBox1.Image = _bitmaps[trackBar1.Value++]; }));
-                }
+					this.BeginInvoke(new Action(() => {
+						Text = $"{i} %";
+						pictureBox1.Image = _bitmaps[trackBar1.Value++];
+					}));
+				}
 
                 _bitmaps.Add(bitmap);
-                this.Invoke(new Action(() => { pictureBox1.Image = _bitmaps[trackBar1.Value++]; }));
+                this.BeginInvoke(new Action(() => { pictureBox1.Image = _bitmaps[trackBar1.Value++]; }));
+                _imageAction = ImageAction.PixelsDeleted;
             }
         }
 
-        private async void blurToolStripMenuItem_Click(object sender, EventArgs e)
+		private async void blurToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			splitContainer1.Panel2Collapsed = true;
+			int value = trackBar1.Value - 1;
+            Bitmap currentBitmap = (Bitmap)_bitmaps[value].Clone() ??  _currentImage!; 
+
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+
+			// Запускаем блюр в фоновом потоке
+			Task<Bitmap> blurTask = Task.Run(() =>
+			{
+				return new GaussianBlur(2000, 2000).Apply(currentBitmap);
+			});
+
+			// Обновляем UI в основном потоке, не блокируя его
+			while (!blurTask.IsCompleted)
+			{
+				Text = $"Processing: {sw.Elapsed.ToString().Remove(10)}";
+				await Task.Delay(1);
+			}
+
+			sw.Stop();
+
+			// Получаем результат блюра и обновляем UI
+			_currentImage = await blurTask;
+
+			// Обновление UI в основном потоке
+			Text = $"Processing complete: {sw.Elapsed.ToString().Remove(10)}";
+			pictureBox1.Image = _currentImage;
+            _imageAction = ImageAction.ImageBlured;
+		}
+
+		private void getTextToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            splitContainer1.Panel2Collapsed = true;
-            currentImage = (Bitmap)new GaussianBlur(2000, 2000).Apply(currentImage);
-            pictureBox1.Image = currentImage;
 
-            MessageBox.Show("Succesfull");
-        }
-
-        private void getTextToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-            var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
-            var img = Pix.LoadFromFile(openFileDialog1.FileName);
-            var page = ocr.Process(img);
-            MessageBox.Show(page.GetText());
+           
 
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            if (pictureBox1.Image != null && saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
                 pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
         }
     }
 }
